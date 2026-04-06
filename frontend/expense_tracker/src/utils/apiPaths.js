61be1e5b4cede9264c6d1ebd292eb766f5ff1f40
Sai Muthu Prasanna R <saiprasannaworks@gmail.com>
2026-04-06 15:54:42 +0530

    api correction

--- frontend/expense_tracker/src/utils/apiPaths.js
+++ frontend/expense_tracker/src/utils/apiPaths.js
@@ -21,7 +21,7 @@ export const API_PATHS ={
     ADD_EXPENSE:"/api/v1/expense/add",
     GET_ALL_EXPENSE:"/api/v1/expense/get",
     DELETE_EXPENSE:(expenseId)=>`/api/v1/expense/${expenseId}`,
-    DOWNLOAD_EXPENSE:`api/v1/expense/downloadexcel`,
+    DOWNLOAD_EXPENSE:`/api/v1/expense/downloadexcel`,
   },
   IMAGE:{
     UPLOAD_IMAGE:"/api/v1/auth/upload-image",
